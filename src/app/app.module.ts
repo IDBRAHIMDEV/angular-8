acacfb66b223c4688baa15f7e4f7ac9db4027819
Mohamed <idbrahimdev@gmail.com>
2020-01-07 09:33:53 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { IntroComponent } from './intro/intro.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    LayoutComponent,
+    NavbarComponent,
+    SidebarComponent,
+    IntroComponent,
+    ContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
